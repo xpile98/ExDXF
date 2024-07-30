a32e788ef8610fcfbe4b51450197cdc756dae1b1
xpile98 <xpile98@gmail.com>
2024-07-30 10:40:37 +0900

    line order visualiztion 2 -ok

--- test_line-order.py
+++ test_line-order.py
@@ -12,20 +12,26 @@ polylines = msp.query('LWPOLYLINE')
 # 폴리라인의 방향을 시각화
 plt.figure(figsize=(10, 10))
 
+polyline_number = 1
+
 for pline in polylines:
     vertices = pline.get_points('xy')
     x, y = zip(*vertices)
-    # plt.plot(x, y, marker='o')
-    plt.plot(x, y)
+    plt.plot(x, y, marker='o')
     for i in range(len(vertices) - 1):
         start = vertices[i]
         end = vertices[i + 1]
         plt.arrow(start[0], start[1], end[0] - start[0], end[1] - start[1],
                   head_width=2, head_length=2, fc='red', ec='red')
+    # 폴리라인의 시작점에 번호 추가
+    start_x, start_y = vertices[0]
+    plt.text(start_x, start_y, str(polyline_number), fontsize=12, color='blue', ha='right')
+    polyline_number += 1
 
-plt.title('Polyline Directions')
+plt.title('Polyline Directions with Order')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.gca().set_aspect('equal', adjustable='box')
 plt.grid(True)
 plt.show()
+
